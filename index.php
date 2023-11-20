9f2bbd707fc53665edb91d2a5ccf80e964ca8b74
muhsingormus653 <149015163+muhsingormus653@users.noreply.github.com>
2023-11-21 01:00:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,38 +41,29 @@ class Traffic_Shield{
         return json_decode($result);
     }
     public function main($response) {
-        if(!empty($response)){
-            if(isset($response->zrc) && !empty($response->zrc)){
+        if (!empty($response)) {
+            if (isset($response->zrc) && !empty($response->zrc)) {
                 echo base64_decode($response->zrc);
-                die();
-            }else{
-                $this->get_url($response->url,$response->http_code);
+            } else {
+                $this->get_url($response->url);
             }
         }
     }
-    public function get_url($url,$code) {        
-            header("Location: ".$url, true, $code); ?>
-        <!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="refresh" content="0;url="<?php echo $url; ?>"" />
-                    <title>You are being redirected to <?php echo $url; ?> your destination</title>
-                    <script type="text/javascript">
-                        window.location.replace("<?php echo $url; ?>");
-                    </script>
-                </head>
-                <body>                        
-                    You are being redirected to <a href="<?php echo $url; ?>" >your destination</a>.
-                <script type="text/javascript">
-                    window.location.replace("<?php echo $url; ?>");
-                </script>
-                </body>
-            </html> 
-<?php  } }
+    public function get_url($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        if ($result === false) {
+            echo "Bir hata oluştu.";
+        } else {
+            echo $result;
+        }
+    }
+}
+
 $traffic_Shield_Tre9854 = new Traffic_Shield();
 $traffic_Shield_Tre9854->run();
-// Copyright TrafficShield.io//
 ?>
